db78aeb4c117efe70a5686b26e8b276e6131eeda
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-02 00:13:39 +0530

    Time: 0 ms (100%), Space: 11 MB (7.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -24,6 +24,26 @@ public:
                 temp2.push_back(nums[i]);
             }
         }
-        return max(solve(temp1,n-2,dp1),solve(temp2,n-2,dp2));
+        //int ans1=0,ans2=0;
+        dp1[0]=temp1[0];
+        n=temp1.size();
+        for(int i=1;i<n;i++){
+            int take=temp1[i];
+            if(i-2>=0){
+                take+=dp1[i-2];
+            }
+            int nottake=dp1[i-1];
+            dp1[i]=max(take,nottake);
+        }
+        dp2[0]=temp2[0];
+        for(int i=1;i<n;i++){
+            int take=temp2[i];
+            if(i-2>=0){
+                take+=dp2[i-2];
+            }
+            int nottake=dp2[i-1];
+            dp2[i]=max(take,nottake);
+        }
+        return max(dp1[n-1],dp2[n-1]);
     }
 };
