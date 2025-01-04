b8121f040201b656af3c36cc6da11296c1a6d195
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-04 13:13:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (6.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-    int dynamic(int i,int j,vector<vector<long>>&dp){
-        if(j==0 && i==0){
-            return 1;
-        }
-        if(j<0 || i<0){
-            return 0;
-        }
-        if( dp[i][j] != -1 ){
-            return dp[i][j];
-        }
-        int u=dynamic(i-1,j,dp);
-        int l=dynamic(i,j-1,dp);
-        return dp[i][j]=(u+l);
-    }
     int uniquePaths(int m, int n) {
         vector<vector<long>>dp(m+1,vector<long>(n+1,-1));
-        //return dynamic(m-1,n-1,dp);
+        //vector<int>prev(n,-1);
         if(m==1&&n==1){
             return m;
         }
-        dp[0][0]=1;
+        //dp[0][0]=1;
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 int u=dp[i-1][j];
