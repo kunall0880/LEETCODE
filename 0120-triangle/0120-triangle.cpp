2c3dc5de2af99529247ef8101e03706090c425ad
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-06 20:39:13 +0530

    Time: 3 ms (14.05%), Space: 13.7 MB (7.97%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,16 +4,19 @@ public:
         int m=nums.size(),n=nums[0].size();
         vector<vector<int>>dp(m,vector<int>(m,-1));
         //return sol(triangle,0,0,m,dp);
+        vector<int>temp(m,-1);
         for(int i=0;i<m;i++){
-            dp[m-1][i]=nums[m-1][i];
+            temp[i]=nums[m-1][i];
         }
         for(int i=m-2;i>=0;i--){
+            vector<int>prev(m,-1);
             for(int j=i;j>=0;j--){
-                    int down=nums[i][j]+dp[i+1][j];
-                    int dia=nums[i][j]+dp[i+1][j+1];
-                    dp[i][j]=min(down,dia);
+                    int down=nums[i][j]+temp[j];
+                    int dia=nums[i][j]+temp[j+1];
+                    prev[j]=min(down,dia);
             }
+            temp=prev;
         }
-        return dp[0][0];
+        return temp[0];
     }
 };
