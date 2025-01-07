2bbec13686ad03666dd8e99ff3d2f2504f23cc26
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-07 11:58:12 +0530

    Time: 5 ms (10.65%), Space: 15.7 MB (5.2%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -6,31 +6,34 @@ public:
             return nums[0][0];
         }
         long ans=INT_MAX;
+        vector<int>prev(m,-1);
         vector<vector<int>>dp(m,vector<int>(n,-1));
         for(int i=0;i<n;i++){
             //dp[n-1][i]=nums[m-1][i];
-            dp[0][i]=nums[0][i];
+            prev[i]=nums[0][i];
         }
         for(int i=1;i<m;i++){
+            vector<int>temp(n,-1);
             for(int j=0;j<n;j++){
                 long long one=nums[i][j];
                 if(j+1<n){
-                    one+=dp[i-1][j+1];
+                    one+=prev[j+1];
                 }else{
                     one=1e9;
                 }
-                long long two=nums[i][j]+dp[i-1][j];
+                long long two=nums[i][j]+prev[j];
                 long long three=nums[i][j];
                 if(j-1>=0){
-                    three+=dp[i-1][j-1];
+                    three+=prev[j-1];
                 }else{
                     three=1e9;
                 }
-                dp[i][j]=min({one,two,three});
+                temp[j]=min({one,two,three});
             }
+            prev=temp;
         }
         for(int i=0;i<n;i++){
-            ans=min((int)ans,dp[n-1][i]);
+            ans=min((int)ans,prev[i]);
         }
         return ans;
     }
