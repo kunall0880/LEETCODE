7677e84eeb8b0a1c4ffe88c507fadbc2866297b5
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-09 09:56:14 +0530

    Time: 23 ms (5.04%), Space: 64 MB (5.04%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,17 +1,55 @@
+struct trieNode{
+    trieNode * child[26];
+    bool eos;
+    int count;
+};
+trieNode * getNode(){
+    trieNode * newNode=new trieNode();
+    newNode->eos=false;
+    newNode->count=0;
+    for(int i=0;i<26;i++){
+        newNode->child[i]=NULL;
+    }
+    return newNode;
+}
+class trie{
+    public:
+    trieNode * root;
+        trie(){
+            root=getNode();
+        }   
+        void insert(string &str){
+            trieNode * p=root;
+            for(auto it : str){
+                int idx=it-'a';
+                if(p->child[idx]==NULL){
+                    p->child[idx]=getNode();
+                }
+                p=p->child[idx];
+                p->count++;
+            }
+            p->eos=true;
+        }
+
+        int search(string &str){
+            trieNode * p=root;
+            for(auto it : str){
+                int idx=it-'a';
+                if(p->child[idx]==NULL){
+                    return 0;
+                }
+                p=p->child[idx];
+            }
+            return p->count;
+        }
+};
 class Solution {
 public:
-    int prefixCount(vector<string>& nums, string pref) {
-        int count=0,n=nums.size(),l1=pref.length();
-        for(int i=0;i<n;i++){
-            int l2=nums[i].length();
-            if(l2<l1){
-                continue;
-            }
-            string str=nums[i].substr(0,l1);
-            if(str==pref){
-                count++;
-            }
+    int prefixCount(vector<string>& words, string pref) {
+        trie Trie;
+        for(auto it : words){
+            Trie.insert(it);
         }
-        return count;
+        return Trie.search(pref);
     }
 };
