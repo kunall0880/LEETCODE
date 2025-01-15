62aae52ba2baac9a3aa790f9d05fc24c66f8d5d7
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-15 20:17:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (40.19%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -2,30 +2,26 @@ class Solution {
 public:
     bool isSet(int& x, int n) { return x & (1 << n); }
     bool setBit(int& x, int n) { return x = x | (1 << n); }
-    bool unsetBit(int& x, int n) { return x = x & (~(1 << n)); }
     int minimizeXor(int num1, int num2) {
-        int x = num1;
+        int x = 0;
 
         int reqset = __builtin_popcount(num2);
-        int currset = __builtin_popcount(x);
-        int bit = 0;
-        if (reqset > currset) {
-            while (currset < reqset) {
-                if (!isSet(x, bit)) {
-                    setBit(x, bit);
-                    currset++;
-                }
-                bit++;
-            }
-        } else if (reqset < currset) {
-            while (reqset < currset) {
-                if (isSet(x, bit)) {
-                    unsetBit(x, bit);
-                    currset--;
-                }
-                bit++;
+        for(int i=32-1;i>=0&&reqset>0;i--){
+            if(isSet(num1,i)){
+                setBit(x,i);
+                reqset--;
             }
         }
+        if(reqset<=0){
+            return x;
+        }
+        for(int i=0;i<32 && reqset>0;i++){
+            if(!isSet(num1,i)){
+                setBit(x,i);
+                reqset--;
+            }
+        }
+
         return x;
     }
 };
