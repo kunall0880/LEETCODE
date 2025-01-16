d224aa13668b4770ef4d90c3f95e1291b77015b4
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-16 12:35:34 +0530

    Time: 0 ms (100%), Space: 64.2 MB (64.63%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -4,18 +4,25 @@ public:
         int ans=0;
         int m=nums1.size();
         int n=nums2.size();
-        unordered_map<int,long long>mp;
-        for(int i=0;i<m;i++){
-            mp[nums1[i]]+=n;
-        }
-        for(int i=0;i<n;i++){
-            mp[nums2[i]]+=m;
-        }
-        for(auto it : mp){
-            if(it.second%2==0){
-                continue;
+        if(n%2==0 && m%2==0){
+            ans=0;
+        }else if(m%2==0){
+            for(int i=0;i<m;i++){
+                ans=ans^nums1[i];
             }
-            ans=ans^it.first;
+        }else if(n%2==0){
+            for(int i=0;i<n;i++){
+                ans=ans^nums2[i];
+            }
+        }else{
+            int first=0,second=0;
+            for(int i=0;i<m;i++){
+                first=first^nums1[i];
+            }
+            for(int i=0;i<n;i++){
+                second=second^nums2[i];
+            }
+            ans=first^second;
         }
         return ans;
     }
