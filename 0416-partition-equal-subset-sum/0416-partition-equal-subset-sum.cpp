08098a52ccc5f0307abef6d9295281de007dcaac
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-17 23:51:11 +0530

    Time: 71 ms (83.79%), Space: 15.6 MB (79.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -7,23 +7,23 @@ public:
             return false;
         }
         int target=(s/2);
-        vector<vector<bool>>dp(n+1,vector<bool>((s/2)+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
+        //vector<vector<bool>>dp(n+1,vector<bool>((s/2)+1,0));
+        vector<int>curr(target+1,0),prev(target+1,0);
+        prev[0]=true;
         if(target>=nums[0]){
-            dp[0][nums[0]]=true;
+            prev[nums[0]]=true;
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<=target;j++){
-                bool nottake=dp[i-1][j];
+                bool nottake=prev[j];
                 bool take=false;
                 if(j>=nums[i]){
-                take=dp[i-1][j-nums[i]];
+                take=prev[j-nums[i]];
                 }
-                dp[i][j]=(take||nottake);
+                curr[j]=(take||nottake);
             }
+            prev=curr;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
