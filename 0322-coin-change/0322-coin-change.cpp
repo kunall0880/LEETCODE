77192e7c16ee468963cd25ec929cc1d9edef46a6
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-21 23:40:29 +0530

    Time: 35 ms (50%), Space: 29.9 MB (41.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int target) {
         int n=coins.size();
-        vector<vector<long long>>dp(n,vector<long long>(target+1,0));
+        //vector<vector<long long>>dp(n,vector<long long>(target+1,0));
         //sort(coins.begin(),coins.end());
+        vector<long long>curr(target+1,0),prev(target+1,0);
         for(int i=0;i<=target;i++){
             if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
+                prev[i]=i/coins[0];
             }else{
-                dp[0][i]=INT_MAX;
+                prev[i]=INT_MAX;
             }
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<=target;j++){
-                long long nottake=0+dp[i-1][j];
+                long long nottake=0+prev[j];
                 long long take=INT_MAX;
                 if(coins[i]<=j){
-                    take=1+dp[i][j-coins[i]];
+                    take=1+curr[j-coins[i]];
                 }
-                dp[i][j]=min(take,nottake);
+                curr[j]=min(take,nottake);
             }
+            prev=curr;
         }
-        return dp[n-1][target]==INT_MAX?-1:dp[n-1][target];
+        return prev[target]==INT_MAX?-1:prev[target];
 }
 };
