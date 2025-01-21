153a24ac165379e1abb3d6af69b21f1a3981815f
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-22 00:12:50 +0530

    Time: 16 ms (58.33%), Space: 25.5 MB (30.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int idx,int ans,int sum,unordered_map<string,int>&dp){
+    int solve(vector<int>& nums,int idx,int ans,int sum,vector<vector<int>>&dp,int state){
         if(idx==nums.size()){
             if(ans==sum){
                 return 1;
             }
             return 0;
         }
-        string key=to_string(idx)+"_"+to_string(sum);
-        if(dp.count(key)){
-            return dp[key];
+        if(dp[idx][sum+state]!=-1){
+            return dp[idx][sum+state];
         }
-        int plus=solve(nums,idx+1,ans,sum+nums[idx],dp);
-        int minus=solve(nums,idx+1,ans,sum-nums[idx],dp);
-        return dp[key]=(plus+minus);
+        int plus=solve(nums,idx+1,ans,sum+nums[idx],dp,state);
+        int minus=solve(nums,idx+1,ans,sum-nums[idx],dp,state);
+        return dp[idx][sum+state]=(plus+minus);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<string,int>dp;
-        return solve(nums,0,target,0,dp);
+        int state=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(n,vector<int>(2*state+1,-1));
+        return solve(nums,0,target,0,dp,state);
     }
 };
