171c454207f419a805f6b84a3b84a37e966b9efe
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-22 22:55:14 +0530

    Time: 31 ms (30.98%), Space: 46 MB (15.69%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& coins,int idx,int sum,vector<vector<int>>&dp){
-        if(idx==0){
-            if(sum%coins[0]==0){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[idx][sum]!=-1){
-            return dp[idx][sum];
-        }
-        int nottake=solve(coins,idx-1,sum,dp);
-        int take=0;
-        if(sum>=coins[idx]){
-            take=solve(coins,idx,sum-coins[idx],dp);
-        }
-        return dp[idx][sum]=(take+nottake);
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return solve(coins,n-1,amount,dp);
+        vector<vector<double>>dp(n,vector<double>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                double nottake=dp[i-1][j];
+                double take=0;
+                if(j>=coins[i]){
+                    take=dp[i][j-coins[i]];
+                }
+                dp[i][j]=(take+nottake);
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
