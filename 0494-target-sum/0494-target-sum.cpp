2582ff851a76c955a5de97ce66bf15fb71410aac
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-22 22:06:19 +0530

    Time: 5 ms (80.24%), Space: 15.7 MB (41.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -9,24 +9,26 @@ public:
         sum=sum-target;
         sum=sum/2;
         vector<vector<int>>dp(n,vector<int>(sum+1,0));
+        vector<int>prev(sum+1,0),curr(sum+1,0);
         if(nums[0]==0){
-            dp[0][0]=2;
+            prev[0]=2;
         }else{
-            dp[0][0]=1;
+            prev[0]=1;
         }
         if(nums[0]!=0 && nums[0]<=sum){
-            dp[0][nums[0]]=1;
+            prev[nums[0]]=1;
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<=sum;j++){
-                int nottake=dp[i-1][j];
+                int nottake=prev[j];
                 int take=0;
                 if(nums[i]<=j){
-                    take=dp[i-1][j-nums[i]];
+                    take=prev[j-nums[i]];
                 }
-                dp[i][j]=(take+nottake);
+                curr[j]=(take+nottake);
             }
+            prev=curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
