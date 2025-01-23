fb75015492728708b2ca05954443bbf7fb6b07d8
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-23 12:39:23 +0530

    Time: 7 ms (31.23%), Space: 26.6 MB (59.58%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int maxrow(vector<vector<int>>& grid,int n){
-        int count=0;
-        for(int i=0;i<grid[0].size();i++){
-            if(grid[n][i]==1){
-                count++;
-            }
-        }
-        return count-1;
-    }
-    int maxcol(vector<vector<int>>& grid,int n){
-        int count=0;
-        for(int i=0;i<grid.size();i++){
-            if(grid[i][n]==1){
-                count++;
-            }
-        }
-        return count-1;
-    }
     int countServers(vector<vector<int>>& grid) {
         int maxi=-1e8;
         int m=grid.size();
         int n=grid[0].size();
         int count=0;
+        vector<int>row(m,0);
+        vector<int>col(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    if((maxrow(grid,i)||maxcol(grid,j))){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    if(row[i]>=2||col[j]>=2){
                         count++;
                     }
                 }
