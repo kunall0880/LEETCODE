3c7c4af81177b5ca6feafa9a7767fccb7d553c43
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-01-28 19:36:43 +0530

    Time: 82 ms (9.6%), Space: 36.6 MB (5.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     vector<vector<int>>dir={{0,1},{0,-1},{1,0},{-1,0}};
-    void dfs(int i,int j,vector<vector<char>>&grid){
+    // void dfs(int i,int j,vector<vector<char>>&grid){
+    //     int m=grid.size(),n=grid[0].size();
+    //     grid[i][j]='0';
+    //     for(auto it : dir){
+    //         int i_=i+it[0];
+    //         int j_=j+it[1];
+    //         if(i_>=0&&i_<m&&j_>=0&&j_<n&&grid[i_][j_]=='1'){
+    //             //grid[i_][j_]=0;
+    //             dfs(i_,j_,grid);
+    //         }
+    //     }
+    // }
+    void bfs(int i,int j,vector<vector<char>>&grid){
         int m=grid.size(),n=grid[0].size();
+        queue<pair<int,int>>que;
         grid[i][j]='0';
-        for(auto it : dir){
+        que.push({i,j});
+        while(!que.empty()){
+            i=que.front().first;
+            j=que.front().second;
+            que.pop();
+            for(auto it : dir){
             int i_=i+it[0];
             int j_=j+it[1];
             if(i_>=0&&i_<m&&j_>=0&&j_<n&&grid[i_][j_]=='1'){
-                //grid[i_][j_]=0;
-                dfs(i_,j_,grid);
+                grid[i_][j_]=0;
+                que.push({i_,j_});
+                }
             }
         }
     }
@@ -20,7 +39,7 @@ public:
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(i,j,grid);
+                    bfs(i,j,grid);
                 }
             }
         }
