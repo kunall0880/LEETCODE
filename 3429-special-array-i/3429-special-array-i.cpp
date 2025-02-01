6f51397ac0c9f9d012656fcfd1bbff79272d20f5
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-01 11:27:03 +0530

    Time: 0 ms (100%), Space: 27.8 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n=nums.size();
         for(int i=1;i<n;i++){
-            if((nums[i-1]%2==0&&nums[i]%2==0)||(nums[i-1]%2==1&&nums[i]%2==1)){
+            if((nums[i-1]&1)==(nums[i]&1)){
                 return false;
             }
         }
