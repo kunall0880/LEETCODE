b011efd1bf969034a888a316c5587cecae870e7e
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-03 08:52:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.88%) - LeetHub

--- 1033-broken-calculator/1033-broken-calculator.cpp
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int brokenCalc(int start, int tar) {
-        int curr=tar;
-        int oper=0;
-        while(curr!=start&&curr>=0){
-            if(curr<start){
-                curr++;
-                oper++;
-            }else if(curr%2==0){
-                curr=curr/2;
-                oper++;
-            }else{
-                curr++;
-                oper++;
-            }
+        if(tar<=start){
+            return start-tar;
         }
-        return oper;
+        if(tar%2==0){
+            return 1+brokenCalc(start,tar/2);
+        }
+        return 1+brokenCalc(start,tar+1);
     }
 };
