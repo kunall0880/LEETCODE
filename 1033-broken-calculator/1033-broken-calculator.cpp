309839e6419738c4c0ce907ed07f4851243a4528
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-03 08:42:22 +0530

    Time: 655 ms (6.71%), Space: 7.7 MB (81.88%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int brokenCalc(int start, int tar) {
+        long long curr=tar;
+        int oper=0;
+        while(curr!=start&&curr>=0){
+            if(curr<start){
+                curr++;
+                oper++;
+            }else if(curr%2==0){
+                curr=curr/2;
+                oper++;
+            }else{
+                curr++;
+                oper++;
+            }
+        }
+        return oper;
+    }
+};
