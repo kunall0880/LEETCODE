3c514a37cfafb3801f426fcf7bd399042da98a61
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-10 14:56:12 +0530

    Time: 14 ms (0.59%), Space: 8.8 MB (94.84%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     vector<int> getRow(int n) {
-        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
-        vector<int>ans;
-        for(int i=0;i<=n;i++){
-            dp[i][i]=1;
-            dp[i][1]=i;
-            dp[i][0]=1;
-        }            
+        vector<int>ans,curr(n+4,0),prev(n+4,0);
+        if(n==0){
+            return {1};
+        }else if(n==1){
+            return {1,1};
+        } 
+        prev[0]=1;
+        prev[1]=1;     
+        n=n+2;     
         for(int i=2;i<=n;i++){
             for(int j=2;j<=i;j++){
-                dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
-                cout<<dp[i][j]<<" ";
+                curr[j]=prev[j]+prev[j-1];
+                cout<<curr[j]<<" ";
             }
+            prev=curr;
             cout<<endl;
         }
         for(int i=0;i<=n;i++){
-            if(dp[n][i]==0){
-                break;
+            if(prev[i]==0){
+                continue;
             }
-            ans.push_back(dp[n][i]);
+            ans.push_back(prev[i]);
         }
         return ans;
     }
 };
-/*
-0 1 2  3 4 5 6
-1 0 0  0 0 0 0   0
-1 1 0  0 0 0 0   1
-1 2 1  0 0 0 0   2
-1 3 3  1 0 0 0   3
-1 4 6  4 1 0 0   4
-1 5 10 10 1 0   5
-1 6 15 20 11 1  6
-*/
