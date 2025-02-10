fde9968e526059fae3da81cd88fd5808d9a6d528
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-10 20:11:50 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.42%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        string str="";
-        int n=s.size(),count=0;
-        for(int i=n-1;i>=0;i--){
+        int n=s.length();
+        int i=0;
+        int j=0;
+        while(i<n){
             if(s[i]>='0'&&s[i]<='9'){
-                count++;
+                j=max(j-1,0);
+            }else{
+                s[j]=s[i];
+                j++;
             }
-            if(s[i]>='a'&&s[i]<='z'&&count==0){
-                str=str+s[i];
-            }else if(s[i]>='a'&&s[i]<='z'){
-                count--;
-                continue;
-            }
+            i++;
         }
-        reverse(str.begin(),str.end());
-        return str;
+        return s.substr(0,j);
     }
 };
