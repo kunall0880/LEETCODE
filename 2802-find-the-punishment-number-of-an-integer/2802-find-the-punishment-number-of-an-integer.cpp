c4d85e3f1eea26fbdaa6702c0a6bdee9990386ec
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-15 23:07:40 +0530

    Time: 762 ms (9.82%), Space: 538.7 MB (8.82%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    bool solve(int idx,string s,int sum,int num){
+    bool solve(int idx,string s,int sum,int num,vector<vector<int>>&dp){
         if(idx==s.length()){
             return sum==num;
         }
         if(sum>num){
             return false;
         }
+        if(dp[idx][sum]!=-1){
+            return dp[idx][sum];
+        }
         bool ans=false;
         for(int j=idx;j<s.length();j++){
             string str=s.substr(idx,j-idx+1);
             int val=stoi(str);
-            ans=ans||solve(j+1,s,sum+val,num);
+            ans=ans||solve(j+1,s,sum+val,num,dp);
             if(ans){
-                return ans;
+                return dp[idx][sum]=ans;
             }
         }
-        return ans;
+        return dp[idx][sum]=ans;
     }
     int punishmentNumber(int n) {
         int sum=0;
         for(int i=1;i<=n;i++){
             int num=i*i;
             string str=to_string(num);
-            if(solve(0,str,0,i)){
+            vector<vector<int>>dp(str.length(),vector<int>(i+1,-1));
+            if(solve(0,str,0,i,dp)){
                 sum=sum+num;
             }
         }
