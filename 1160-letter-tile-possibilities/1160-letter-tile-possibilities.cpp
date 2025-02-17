5f5882e21af7ad905cfaf819a5cc56525ca6fa9e
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-17 18:38:37 +0530

    Time: 4 ms (62.18%), Space: 8 MB (84.16%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    void solve(string & tiles,unordered_set<string> & st,vector<bool> & used,string & curr,int n){
-        st.insert(curr);
-        for(int i=0;i<n;i++){
-            if(used[i]){
+    // void solve(string & tiles,unordered_set<string> & st,vector<bool> & used,string & curr,int n){
+    //     st.insert(curr);
+    //     for(int i=0;i<n;i++){
+    //         if(used[i]){
+    //             continue;
+    //         }
+    //         used[i]=true;
+    //         curr.push_back(tiles[i]);
+    //         solve(tiles,st,used,curr,n);
+    //         used[i]=false;
+    //         curr.pop_back();
+    //     }
+    // }
+    //     int n=tiles.length();
+    //     vector<bool>used(n,false);
+    //     unordered_set<string>st;
+    //     string curr="";
+    //     solve(tiles,st,used,curr,n);
+    //     return (st.size()-1);
+    void solve(vector<int>&nums,int & count){
+        count++;
+        for(int i=0;i<26;i++){
+            if(nums[i]==0){
                 continue;
             }
-            used[i]=true;
-            curr.push_back(tiles[i]);
-            solve(tiles,st,used,curr,n);
-            used[i]=false;
-            curr.pop_back();
+            nums[i]--;
+            solve(nums,count);
+            nums[i]++;
         }
     }
     int numTilePossibilities(string tiles) {
-        int n=tiles.length();
-        vector<bool>used(n,false);
-        unordered_set<string>st;
-        string curr="";
-        solve(tiles,st,used,curr,n);
-        return (st.size()-1);
+        vector<int>nums(26,0);
+        for(auto & ch : tiles){
+            nums[ch-'A']++;
+        }
+        int count=0;
+        solve(nums,count);
+        return count-1;
     }
 };
