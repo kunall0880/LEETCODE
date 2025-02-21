2b32d6c95ec366a5b0893a22c83353c4f652df16
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-02-21 08:09:17 +0530

    Time: 1135 ms (5.05%), Space: 388.8 MB (5.98%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    void BFS(TreeNode * root){
+        queue<TreeNode*>que;
+        que.push(root);
+        root->val=0;
+        int x=0;
+        while(!que.empty()){
+            TreeNode * temp=que.front();
+            que.pop();
+            x=temp->val;
+            if(temp->right!=NULL){
+                temp->right->val=2*x+2;
+                que.push(temp->right);
+            }
+            if(temp->left){
+                temp->left->val=x*2+1;
+                que.push(temp->left);
+            }
+        }
+    }
+    bool BFSser(TreeNode * root,int ele){
+        queue<TreeNode*>que;
+        que.push(root);
+        while(!que.empty()){
+            TreeNode * temp=que.front();
+            que.pop();
+            int x=temp->val;
+            if(x==ele){
+                return true;
+            }
+            if(temp->right!=NULL){
+                que.push(temp->right);
+            }
+            if(temp->left){
+                que.push(temp->left);
+            }
+        }
+        return false;
+    }
+    TreeNode * main;
+    FindElements(TreeNode* root) {
+        main=root;
+        BFS(root);
+    }
+    
+    bool find(int target) {
+       return BFSser(main,target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
