79f8889ff8bbb2392d086674074251c71e88f205
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-03-05 21:02:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-    if(n==1){
-        return 1;
-    }
-    long long ans=1;
-    for(int i=1;i<n;i++){
-       long long power=4*i;
-       ans=ans+power;
-    }
-    return ans;   
+    return 1+(long long)(2*(long long)(n-1)*n);  
+    // if(n==1){
+    //     return 1;
+    // }
+    // long long ans=1;
+    // for(int i=1;i<n;i++){
+    //    long long power=4*i;
+    //    ans=ans+power;
+    // }
+    // return ans;   
     }
 };
