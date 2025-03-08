3d89668009446316363f7ea1e21f9f0c4dc2089d
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-03-08 23:49:42 +0530

    Time: 0 ms (100%), Space: 8.3 MB (92.08%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -19,3 +19,17 @@ public:
         return mini;
     }
 };
+
+
+// int minimumRecolors(string nums, int k) {
+//         int n=nums.size(),mini=INT_MAX,w=0;
+//         for(int i=0;i<n-k+1;i++){
+//             w=0;
+//             for(int j=i;j<i+k;j++){
+//                 if(nums[j]=='W'){
+//                     w++;
+//                 }
+//             }
+//             mini=min(mini,w);
+//         }
+//         return mini;
