ef6d37f5f4dcbce58894975b9ff088608ac0eb7a
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-03-10 01:53:33 +0530

    Time: 73 ms (50.84%), Space: 102.6 MB (91.98%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,30 +1,55 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& nums, int k) {
-         int n=nums.size();
-        for(int i=0;i<k-1;i++){
-            nums.push_back(nums[i]);
-        }
-        int count=0;
-        int i=0,j=1;
-        int N=n+(k-1);
-        while(j<N){
-            if(nums[j-1]==nums[j]){
-                i=j;
-                j++;
+        int n=nums.size(),len=1;
+        int last=nums[0],count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==last){
+                len=1;
+                last=nums[i];
                 continue;
             }
-            if(j-i+1==k){
-                i++;
+            len++;
+            if(len>=k){
                 count++;
             }
-            j++;
+            last=nums[i];
+        }
+        for(int i=0;i<k-1;i++){
+            if(nums[i]==last){
+                break;
+            }
+            len++;
+            if(len>=k){
+                count++;
+            }
+            last=nums[i];
         }
         return count;
     }
 };
 
 
+// int n=nums.size();
+//         for(int i=0;i<k-1;i++){
+//             nums.push_back(nums[i]);
+//         }
+//         int count=0;
+//         int i=0,j=1;
+//         int N=n+(k-1);
+//         while(j<N){
+//             if(nums[j-1]==nums[j]){
+//                 i=j;
+//                 j++;
+//                 continue;
+//             }
+//             if(j-i+1==k){
+//                 i++;
+//                 count++;
+//             }
+//             j++;
+//         }
+//         return count;
 
 
 // TLE  
