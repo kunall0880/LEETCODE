e958f3368f116ebddaca6f735e960d62dbdb3c31
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-04-01 08:51:28 +0530

    Time: 19 ms (10.76%), Space: 131 MB (8.52%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    // long long solve(vector<vector<int>>& nums,int idx,int n,vector<long long>&dp){
-    //     if(idx>=n){
-    //         return 0;
-    //     }
-    //     if(dp[idx]!=-1){
-    //         return dp[idx];
-    //     }
-    //     long long nottake=0+solve(nums,idx+1,n,dp);
-    //     long long take=nums[idx][0]+solve(nums,idx+nums[idx][1]+1,n,dp);
-    //     return dp[idx]=max(nottake,take);
-    // }
-    long long mostPoints(vector<vector<int>>& nums) {
-        int n=nums.size();
-        vector<long long>dp(2*n+1,0);
-        for(int idx=n-1;idx>=0;idx--){
-            long long nottake=0+dp[idx+1];
-            long long take=nums[idx][0];
-            if(idx+nums[idx][1]+1<n){
-                take+=dp[idx+nums[idx][1]+1];
-            }
-            dp[idx]=max(take,nottake);
+    long long solve(vector<vector<int>>& questions,int idx, vector<long long>&dp){
+        if(idx>=questions.size()){
+            return 0;
         }
-        return dp[0];
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        long long take = questions[idx][0]+solve(questions,idx+questions[idx][1]+1,dp);
+        long long nottake = solve(questions,idx+1,dp);
+        return dp[idx]=max(take,nottake);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long>dp(n+n,-1);
+        return solve(questions,0,dp);
     }
 };
