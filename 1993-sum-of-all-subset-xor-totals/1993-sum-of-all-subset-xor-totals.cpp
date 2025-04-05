9e800e9caa7538e192f3f95b4b3bd25bfddd2aa9
kunall0880 <142335394+kunall0880@users.noreply.github.com>
2025-04-06 00:47:55 +0530

    Time: 0 ms (100%), Space: 9.1 MB (71.99%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    void gensub(vector<vector<int>>&ans,vector<int>&nums,int idx,vector<int>curr){
+    int gensub(vector<int>&nums,int idx,int x){
         if(idx==nums.size()){
-            ans.push_back(curr);
-            return ;
+            return x;
         }
-        curr.push_back(nums[idx]);
-        gensub(ans,nums,idx+1,curr);
-        curr.pop_back();
-        gensub(ans,nums,idx+1,curr);
+        int take=gensub(nums,idx+1,x^nums[idx]);
+        int nottake=gensub(nums,idx+1,x);
+        return (take+nottake);
     }
     int subsetXORSum(vector<int>& nums) {
-        int ans=0,n=nums.size();
-        vector<vector<int>>all;
-        vector<int>sub;
-        gensub(all,nums,0,sub);
-        int sol=0;
-        for(auto it : all){
-            int x=0;
-            for(auto ele : it){
-                x=x^ele;
-            }
-            sol+=x;
-        }
-        return sol;
+        return gensub(nums,0,0);
+        
     }
 };
